8ab7d9a0665af0e7463697d427e44c73185619d3
tapin13 <tapin13@gmail.com>
2012-03-21 02:07:10 +0200

    done edit main.cpp

--- main.cpp
+++ main.cpp
@@ -37,7 +37,7 @@ int main(int argc, char** argv) {
     glutInitDisplayMode(GLUT_SINGLE|GLUT_RGB);
     glutInitWindowSize(WinWid, WinHei);
     glutInitWindowPosition(100, 200);
-    glutCreateWindow("Lesson 01");
+    glutCreateWindow("Lesson 02");
     
     glutDisplayFunc(Draw);
     Initsialaize();
